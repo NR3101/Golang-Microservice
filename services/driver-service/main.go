5c58669d397b99cfa908655a44f3b7551cc36035
NR3101 <neeraj2000rai@gmail.com>
2026-04-07 20:49:16 +0530

    Created initial scaffold for driver service

--- services/driver-service/main.go
+++ services/driver-service/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"ride-sharing/shared/env"
+	"syscall"
+
+	grpcserver "google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = env.GetString("GRPC_ADDR", ":9092")
+)
 
 func main() {
-	fmt.Println("Hello, this is the driver service!")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+		cancel()
+	}()
+
+	lis, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	service := NewService()
+
+	// Start gRPC server
+	grpcServer := grpcserver.NewServer()
+
+	NewGRPCHandler(grpcServer, service)
+
+	log.Printf("starting gRPC server Driver Service on %s", lis.Addr().String())
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Printf("failed to serve: %v", err)
+			cancel()
+		}
+	}()
+
+	// Wait for shutdown signal
+	<-ctx.Done()
+	log.Printf("shutting down gRPC server Driver Service on %s", lis.Addr().String())
+	grpcServer.GracefulStop()
 }
